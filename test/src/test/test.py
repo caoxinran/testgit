d0a63dad064e7b907a9aa71f30cebbfb77d8c6cb
caoxinran <1532438522@qq.com>
2017-04-21 20:50:12 +0800

    test

--- test/src/test/test.py
+++ test/src/test/test.py
@@ -1,7 +1,20 @@
 #coding:utf-8
 '''
-Created on 2017Äê4ÔÂ19ÈÕ
+Created on 2017ï¿½ï¿½4ï¿½ï¿½19ï¿½ï¿½
 
 @author: caoxinran
 '''
-print 'hello world!'
+num_inc_list=range(30);
+intial_num=0;
+list_lenth=9;
+sample_list=[intial_num for i in range(20)];
+sample_list1=[intial_num]*list_lenth;
+
+print num_inc_list;
+print sample_list;
+print sample_list1;
+print sample_list[-1];
+print len(sample_list);
+
+
+
